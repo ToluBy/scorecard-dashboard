acf90c92be0a77f6fe7964877e2fed370e243883
Durojaiye <TOLU.DUROJAIYE@VIZIENTINC.COM>
2025-06-02 13:50:19 -0500

    Remove Databricks access token

--- dashboard-app/src/environment.ts
+++ dashboard-app/src/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     production: false,
     databricks: {
       workspaceUrl: 'https://adb-986067428197724.4.azuredatabricks.net/sql/1.0/warehouses/881935ad0bc2af0c',
-      accessToken: 'REDACTED'
+      accessToken: 'REPLACE_ME'
     }
   };
